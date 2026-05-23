731d32ddf77171a36e7849330dee00d94abcd83d
Achmad Zulfadhlillah <email-zulfadhlillah@gmail.com>
2026-05-23 21:27:52 +0700

    Fix: UserFactory pakai Faker Factory create

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,30 +12,19 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => \Faker\Factory::create()->name(),
+            'email' => \Faker\Factory::create()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
